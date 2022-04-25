a62a5d9d49f225e997a98aef61a547184c8cf92b
JesusGandolfo <104355962+JesusGandolfo@users.noreply.github.com>
2022-04-25 12:09:57 +0200

    Add files via upload

---
+++ practica-gandolfo.php
@@ -0,0 +1,37 @@
+<?php
+/**
+*Devuelve la suma de dos números
+*
+*@author Jesus Gandolfo Sánchez
+*@version 1.1
+*@param integer $num1 Primer número
+*@param integer $num2 Segundo número
+*@return integer $resultado Devuelve la suma de los números
+*{@internal Anotación solo para desarrolladores}
+*@access public
+*/
+   function sumar($num1, $num2){
+      	$resultado = $num1 + $num2;
+        return $resultado;
+   }
+/**
+*Devuelve la multiplicación de dos números
+*@author Jesus Gandolfo Sánchez
+*@version 1.1
+*@param integer $num1 primer número de la multiplicación
+*@param integer $num2 segundo número de la multiplicación
+*@return integer $resultado Devuelve la multiplicación de dos números
+*{@internal Anotación solo para desarrolladores}
+*@access public
+*/
+   function multiplicar ( $num1, $num2 ) {
+	$resultado=$num1 * $num2;
+	return $resultado;
+   }
+
+   $resultado_de_suma = sumar(20, 7);
+   echo 'La suma es: '. $resultado_de_suma;
+   echo '<br />';
+   $resultado = multiplicar (2, 9 ) ;
+   echo'La multiplicación es: '. $resultado;	
+?>
